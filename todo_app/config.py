1378ce0c76254e05513b6a3b54e35c1c8cf49070
Sadegh <fallahi.s2001@gmail.com>
2025-11-11 20:30:23 +0330

    feat: Update environment configuration with database settings

--- todo_app/config.py
+++ todo_app/config.py
@@ -9,3 +9,9 @@ load_dotenv(dotenv_path=env_path)
 class Config:
     """Application configuration from environment variables"""
     MAX_NUMBER_OF_PROJECTS = int(os.getenv('MAX_NUMBER_OF_PROJECTS', 10))
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
